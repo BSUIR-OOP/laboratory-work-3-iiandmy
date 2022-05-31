e10115c54cbe04d6bf7cce7ea7fa73417b620ca4
iia.m.y <frezhgrand@gmail.com>
2022-06-01 02:06:10 +0300

    handle result of parsing

--- SwiftyJSON/Controller/SerializationController.swift
+++ SwiftyJSON/Controller/SerializationController.swift
@@ -20,11 +20,28 @@ class SerializationController: UIViewController {
     
     @IBAction func deserializeButtonPressed(_ sender: Any) {
         guard let readedJson = FileService.instance.readFromFile() else { return }
-        guard let result = SerializationService.deserialize(jsonString: readedJson) else { return }
+        let result = SerializationService.deserialize(jsonString: readedJson)
+        
+        switch result {
+        case .error:
+            showAllert(message: "Error!", description: "Something went wrong.")
+        case .empty:
+            showAllert(message: "Warning!", description: "Json file you trying to parse is empty")
+        case .success(let result):
+            result.forEach { transport in
+                TransportStorage.instance.transport.append(transport as! Transport)
+            }
+        }
     }
     
     @IBAction func clearFilePressed(_ sender: Any) {
         FileService.instance.clearFile()
+    }
+    
+    func showAllert(message: String, description: String) {
+        let alert = UIAlertController(title: message, message: description, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
